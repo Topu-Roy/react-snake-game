bee191bde665d0b76290be15ae3f89f4e93212c4
Topu-Roy <topuroy143333@gmail.com>
2023-08-21 13:11:15 +0600

    made the grid & and rendered the food and the snake body inside the grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,64 @@
+"use client";
+import { useState } from "react";
+
 export default function Home() {
+  // * Variables
+  let totalGridSize = 20;
+
+  let initialSnakePosition = [
+    { x: totalGridSize / 2, y: totalGridSize / 2 },
+    { x: totalGridSize / 2 + 1, y: totalGridSize / 2 },
+  ];
+
+  // * States
+
+  const [foodPosition, setFoodPosition] = useState({
+    x: totalGridSize / 2 - 5,
+    y: totalGridSize / 2,
+  });
+  const [snakePosition, setSnakePosition] = useState(initialSnakePosition);
+
+  // * Functions
+  function renderGrid() {
+    type CellType = JSX.Element[];
+    let cellArray: CellType = [];
+
+    for (let row = 0; row < totalGridSize; row++) {
+      for (let col = 0; col < totalGridSize; col++) {
+        let className = "bg-gray-500 w-full h-full";
+
+        // * Checking if the current cell should render the food or snake
+        let isFoodHere = foodPosition.x === row && foodPosition.y === col;
+        let isSnakeHere = snakePosition.some(
+          (item) => item.x === row && item.y === col
+        );
+
+        // * Rendering the food and snake body
+        if (isFoodHere) {
+          className = `${className} bg-green-500`;
+        }
+
+        if (isSnakeHere) {
+          className = `${className} bg-red-500`;
+        }
+
+        let cell = <div className={className} key={`${row}+${col}`} />;
+        cellArray.push(cell);
+      }
+    }
+
+    return cellArray;
+  }
+
   return (
-    <main className="flex min-h-screen flex-col justify-center items-center p-24">
-      <div className="max-w-5xl w-full items-center justify-center font-mono text-sm lg:flex">
-        <p>Hello Snake Game</p>
+    <main className="flex bg-gray-800 min-h-screen flex-col justify-start items-center p-24">
+      <p>
+        Score: <span>0</span>
+      </p>
+
+      {/* Snake Board */}
+      <div className="h-32 w-32 grid Grid_Custom_Classes gap-[1px]">
+        {renderGrid()}
       </div>
     </main>
   );
