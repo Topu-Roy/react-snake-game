23f4a3c0d2cda0fc5f9f11ba2356e55309d27c24
Topu-Roy <topuroy143333@gmail.com>
2023-09-05 00:38:18 +0600

    added my image & added shadow to the navbar

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,16 @@
+import Image from 'next/image'
 import React from 'react'
 
 function AboutPage() {
     return (
-        <div>AboutPage</div>
+        <>
+            <section className='max-w-7xl flex justify-between items-center mx-auto mt-8'>
+                <div className="w-full p-4">
+                    <Image src={'/topu.jpg'} alt='topu roy' height={1080} width={1080} className='rounded-lg drop-shadow-xl filter grayscale hover:grayscale-0 transition-all duration-200' />
+                </div>
+                <div className="bg-yellow-200 w-full"></div>
+            </section>
+        </>
     )
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 function NavBar() {
     return (
-        <header className='w-full bg-gray-900 py-4 px-3 sm:px-6 md:px-8 lg:px-6 xl:px-16'>
+        <header className='w-full bg-gray-900 py-4 px-3 sm:px-6 md:px-8 lg:px-6 xl:px-16 shadow-md'>
             <nav className='flex justify-between items-center w-full max-w-7xl mx-auto'>
                 <Link href={'/'} className='flex justify-center items-center gap-2'>
                     <Image src={'/snake-logo.png'} alt='snake game by topu' height={35} width={35} />
