c9a3788ab6db9f0e5216e474e9dd5d3b0fc5b0f3
Topu-Roy <topuroy143333@gmail.com>
2023-09-06 13:21:25 +0600

    moved the objects to it's own separate file

---
+++ src/app/about/objects.ts
@@ -0,0 +1,170 @@
+export type TechObjType = {
+  name: string;
+  image: string;
+}[];
+
+export const mainFrameworks: TechObjType = [
+  {
+    name: "NextJS",
+    image: "/tech/next.svg",
+  },
+  {
+    name: "ExpressJS",
+    image: "/tech/express.png",
+  },
+];
+export const styling: TechObjType = [
+  {
+    name: "TailwindCSS",
+    image: "/tech/tailwindcss.png",
+  },
+  {
+    name: "Shadcn/UI",
+    image: "/tech/shadcn-ui.png",
+  },
+];
+export const uiux: TechObjType = [
+  {
+    name: "Figma",
+    image: "/tech/figma.png",
+  },
+  {
+    name: "Photoshop",
+    image: "/tech/photoshop.png",
+  },
+];
+export const misc: TechObjType = [
+  {
+    name: "TypeScript",
+    image: "/tech/typescript.png",
+  },
+  {
+    name: "Firebase",
+    image: "/tech/firebase.png",
+  },
+  {
+    name: "TRPC",
+    image: "/tech/trpc.png",
+  },
+  {
+    name: "ZOD",
+    image: "/tech/zod.png",
+  },
+  {
+    name: "React Hook Forms",
+    image: "/tech/react-hook-forms.png",
+  },
+
+  {
+    name: "Zustand",
+    image: "/tech/zustand.png",
+  },
+  {
+    name: "ChatGPT",
+    image: "/tech/chatgpt.png",
+  },
+  {
+    name: "TenStack Query",
+    image: "/tech/tenstack-query.webp",
+  },
+  {
+    name: "Prisma",
+    image: "/tech/prisma.webp",
+  },
+  {
+    name: "Drizzle",
+    image: "/tech/drizzle.png",
+  },
+  {
+    name: "NextAuth",
+    image: "/tech/nextauth.png",
+  },
+  {
+    name: "ClarkProvider",
+    image: "/tech/clark.png",
+  },
+];
+export const coreTechnologies: TechObjType = [
+  {
+    name: "HTML",
+    image: "/tech/html.png",
+  },
+  {
+    name: "CSS",
+    image: "/tech/css.png",
+  },
+  {
+    name: "JavaScript",
+    image: "/tech/javascript.png",
+  },
+  {
+    name: "React",
+    image: "/tech/react.png",
+  },
+  {
+    name: "NodeJS",
+    image: "/tech/nodejs.jpg",
+  },
+  {
+    name: "Git",
+    image: "/tech/git.png",
+  },
+  {
+    name: "GitHub",
+    image: "/tech/github.png",
+  },
+  {
+    name: "Python",
+    image: "/tech/python.png",
+  },
+  {
+    name: "MongoDB",
+    image: "/tech/mongodb.png",
+  },
+];
+export const interestedToLearn: TechObjType = [
+  {
+    name: "GoLang",
+    image: "/tech/go.png",
+  },
+  {
+    name: "React Native",
+    image: "/tech/react-native.png",
+  },
+  {
+    name: "PostgreSQL",
+    image: "/tech/postgresql.png",
+  },
+  {
+    name: "Rust",
+    image: "/tech/rust.png",
+  },
+  {
+    name: "SvelteKit",
+    image: "/tech/svelte.jpg",
+  },
+  {
+    name: "Linux",
+    image: "/tech/linux.png",
+  },
+  {
+    name: "Machine Learning",
+    image: "/tech/ml.png",
+  },
+  {
+    name: "Artificial Intelligence",
+    image: "/tech/ai.jpg",
+  },
+];
+export const operatingSystem: TechObjType = [
+  {
+    name: "Windows 11 Pro",
+    image: "/tech/windows11.jpg",
+  },
+];
+export const textEditor: TechObjType = [
+  {
+    name: "Visual Studio Code",
+    image: "/tech/vscode.png",
+  },
+];
