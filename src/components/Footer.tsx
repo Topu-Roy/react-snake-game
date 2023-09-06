6347b44acf7fc2dd76479ed4d0bbded1cdf7849b
Topu-Roy <topuroy143333@gmail.com>
2023-09-06 16:10:46 +0600

    added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer className='bg-gray-700'>
+            <section className="max-w-7xl mx-auto py-8 mt-8">
+                <p className='text-center w-full text-lg text-white/70'>
+                    Made With ❤️ By Topu Roy
+                </p>
+            </section>
+        </footer>
+    )
+}
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,13 +4,13 @@ import React from 'react'
 
 function NavBar() {
     return (
-        <header className='w-full bg-gray-900 py-4 px-3 sm:px-6 md:px-8 lg:px-6 xl:px-16 shadow-md'>
+        <header className='w-full  py-4 px-3 sm:px-6 md:px-8 lg:px-6 xl:px-16 shadow-md sticky top-0 left-0 right-0 z-10 bg-gray-900/80 backdrop-blur-sm'>
             <nav className='flex justify-between items-center w-full max-w-7xl mx-auto'>
                 <Link href={'/'} className='flex justify-center items-center gap-2'>
                     <Image src={'/snake-logo.png'} alt='snake game by topu' height={35} width={35} />
-                    <h2 className='font-bold sm:text-lg text-slate-400'>Snake Game</h2>
+                    <h2 className='font-bold sm:text-lg text-slate-200'>Snake Game</h2>
                 </Link>
-                <ul className='flex justify-center items-center gap-4 font-medium text-base text-slate-400'>
+                <ul className='flex justify-center items-center gap-4 font-medium text-base text-slate-300'>
                     <li className=""><Link href={'/'}>Home</Link></li>
                     <li className=""><Link href={'/about'}>About</Link></li>
                     <li className=""><Link href={'/contact'}>Contact</Link></li>
