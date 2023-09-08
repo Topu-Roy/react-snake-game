9c05f9e75f7b1e17c846521856279846a5e3c8a0
Topu-Roy <topuroy143333@gmail.com>
2023-09-08 19:54:23 +0600

    fixed footer issue

--- src/app/snake-game/page.tsx
+++ src/app/snake-game/page.tsx
@@ -3,7 +3,7 @@ import Game from '@/components/Game'
 
 function SnakeGame() {
     return (
-        <div className="flex max-w-7xl min-h-screen md:min-h-max mx-auto flex-col justify-start items-center">
+        <div className="flex relative max-w-7xl min-h-screen md:min-h-max mx-auto flex-col justify-start items-center">
             <Game />
             <Footer position='fixed' />
         </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React from "react";
 
 type Position = {
-    position: 'fixed' | 'nofixed'
-}
+    position: "fixed" | "nofixed";
+};
 
 export default function Footer({ position }: Position) {
     return (
-        <footer className={`bg-gray-700 md:${position} md:bottom-0 md:left-0 md:right-0`}>
-            <section className="max-w-7xl mx-auto py-4 md:py-4">
-                <p className='text-center w-full text-sm md:text-base text-white/70'>
-                    Made With ❤️ By Topu Roy
-                </p>
-            </section>
-        </footer>
-    )
+        <>
+            {position === "fixed" ? (
+                <footer className={`bg-gray-700 fixed bottom-0 left-0 right-0 w-full`}>
+                    <section className="max-w-7xl mx-auto py-4 md:py-4">
+                        <p className="text-center w-full text-sm md:text-base text-white/70">
+                            Made With ❤️ By Topu Roy
+                        </p>
+                    </section>
+                </footer>
+            ) : (
+                <footer className={`bg-gray-700 w-full`}>
+                    <section className="max-w-7xl mx-auto py-4 md:py-4">
+                        <p className="text-center w-full text-sm md:text-base text-white/70">
+                            Made With ❤️ By Topu Roy
+                        </p>
+                    </section>
+                </footer>
+            )}
+        </>
+    );
 }
